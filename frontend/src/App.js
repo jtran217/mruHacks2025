721b035cc16ffc343ce48de019a14bf1e2672b38
dpham001 <dpham331@mtroyal.ca>
2025-10-04 16:10:16 -0600

    added front end with bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import { useState } from 'react';
+import NewGame from './NewGame';
+import JoinGame from './JoinGame';
+
+function HomePage() {
+  const [view, setView] = useState(null);
+
+  const renderNewGameButton = () => (
+    <button className="btn btn-success" onClick={() => setView('new')}> 
+      New Game 
+    </button>
   );
+
+  const renderJoinButton = () => (
+    <button className="btn btn-primary" onClick={() => setView('join')}> 
+      Join Game 
+    </button>
+  );
+
+return (
+  <div className="container text-center mt-5">
+    {view == null && (
+      <>
+    <h1 className="mb-4">Sausage Dungeon Links</h1>
+
+    <div  className="d-flex justify-content-center gap-3 mb-4">
+      {renderNewGameButton()}
+      {renderJoinButton()}
+
+    </div>
+    </>
+
+    )}
+
+    <div className="d-flex justify-content-center">
+      <div className="w-50">
+      {view === 'new' && <NewGame />}
+      {view === 'join' && <JoinGame/>}
+      </div>
+    </div>
+  </div>
+
+);
+
+
 }
 
-export default App;
+export default HomePage;
+
+  
---
+++ frontend/src/NewGame.js
@@ -0,0 +1,12 @@
+import './NewGame.css';
+
+function NewGame() {
+  return (
+    <div className= 'NewGame'>
+        <h1> Join Game With This Code</h1>
+        <h2> CODE CODE CODE</h2>
+    </div>
+  )
+}
+
+export default NewGame;
