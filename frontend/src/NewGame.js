0a5c73d45a3177d83bd285be1a7f5a15c694174b
Ethan <ethan8lime@gmail.com>
2025-10-04 19:36:00 -0600

    Updated CSS styling for all pages

--- frontend/src/NewGame.js
+++ frontend/src/NewGame.js
@@ -3,7 +3,7 @@ import './NewGame.css';
 function NewGame() {
   return (
     <div className= 'NewGame'>
-        <h1> Join Game With This Code</h1>
+        <h1> Join Game With This Code:</h1>
         <h2> CODE CODE CODE</h2>
     </div>
   )
